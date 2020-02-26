f431e47f59764f998c69b6d98dd73c30f38d8fd9
webyom <webyom@gmail.com>
2020-02-26 10:29:28 +0800

    0.50.0

--- includes/global.inc.ts
+++ includes/global.inc.ts
@@ -5,13 +5,13 @@ const G: Record<string, any> = {
   ENV: '', // inject at runtime
   BUILD_TIME: '%{{BUILD_TIME}}%',
   APP_VERSION: '%{{APP_VERSION}}%',
-  BASE: '//' + location.hostname + (location.port ? ':' + location.port : ''),
 };
 
 Object.assign(G, {
   __RUNTIME_CONFIG_INJECT__: 1,
 });
 
+G.base = '//' + location.hostname + (location.port ? ':' + location.port : '');
 if (!G.cdnBase) {
-  G.cdnBase = G.BASE;
+  G.cdnBase = G.base;
 }
