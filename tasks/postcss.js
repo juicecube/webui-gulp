f1a6690b9226254be92a4b48a5940faf0044630b
webyom <webyom@gmail.com>
2020-03-25 15:01:06 +0800

    0.57.0

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -1,10 +1,11 @@
-const gulp = require('../').gulp(),
+const path = require('path'),
+  gulp = require('../').gulp(),
   util = require('./util'),
   lazyTasks = require('./lazy-tasks');
 
 gulp.task('postcss', function() {
   return gulp
-    .src([util.getWorkingDir('build') + '/**/*.css'])
+    .src([util.getWorkingDir('build') + '/**/*.css'], { base: path.resolve('build') })
     .pipe(lazyTasks.lazyPostcssTask())
     .pipe(gulp.dest('build'));
 });
