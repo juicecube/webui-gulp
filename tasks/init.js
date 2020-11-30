337402bdc0295fab554b7849ef723684ef4bd1c0
webyom <webyom@gmail.com>
2020-11-30 18:29:34 +0800

    0.76.1

--- tasks/init.js
+++ tasks/init.js
@@ -4,6 +4,7 @@ gulp.task('copy', function() {
   return gulp
     .src([
       'src/robots.txt',
+      'src/MP_verify_*.txt',
       'src/**/*.+(jpg|jpeg|gif|png|otf|eot|svg|ttf|woff|woff2|ico|mp3|swf)',
       'src/**/_vendor/**/**',
       '!src/**/*.+(less|scss)',
